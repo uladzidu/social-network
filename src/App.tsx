591782e403639c38c40136edc23c64fe8b8c92d5
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-11-08 15:10:31 +0100

    new file Technologies.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'
+import {Technologies} from './Technologies';
 
 
 export function App() {
     return (
         <div>
             <h1>hello</h1>
+            <Technologies/>
         </div>
     )
 }
---
+++ src/Technologies.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export const Technologies = () => {
+    return (
+            <div>
+                <ul>
+                    <li>css</li>
+                    <li>cssss</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+    )
+}
