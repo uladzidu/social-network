68d441b6ad17c2aa27304a2ff196b47685230d51
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-11-29 03:56:57 +0100

    29.11 Data

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -39,25 +39,20 @@ export const Dialogs = (props: propsType) => {
 
     let messagesData = [
         {id: 1, message: 'Hi'},
-        {id: 2, message:},
+        {id: 2, message: 'How are u?'},
         {id: 3, message: 'Good Job!'},
     ]
 
+    let mapedMessagesData = messagesData.map(newMessage => <Message message={newMessage.message}/>)
+    let mapedDialogs = dialogsData.map(dialog => <DialogItem id={dialog.id} name={dialog.name}/>)
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem id ={dialogsData[0].id} name={dialogsData[0].name}  />
-                <DialogItem id ={dialogsData[1].id} name={dialogsData[1].name}  />
-                <DialogItem id ={dialogsData[2].id} name={dialogsData[2].name}  />
-                <DialogItem id ={dialogsData[3].id} name={dialogsData[3].name}  />
-                <DialogItem id ={dialogsData[4].id} name={dialogsData[4].name}  />
+                {mapedDialogs}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} />
-
+                {mapedMessagesData}
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,6 +4,14 @@ import s from './MyPosts.module.css'
 
 
 export const MyPosts = () => {
+
+    let postData = [
+        {id: 1, postMessage: 'Hi, how are you', likes: 5},
+        {id: 2, postMessage: 'It\'s my first post', likes: 15}
+    ]
+
+    let mappedPost = postData.map(post => <Post message={post.postMessage} likes={post.likes}/>)
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -16,8 +24,7 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message="Hi, how are you" likes={5}/>
-                <Post message="It's my first post" likes={15}/>
+                {mappedPost}
             </div>
         </div>
     )
